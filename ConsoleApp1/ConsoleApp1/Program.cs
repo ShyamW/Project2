0f81c51177a289eab78077bebcd9c42d6981367b
Shyam Thiagarajan <shyam.thiagarajan@gmail.com>
2018-01-22 13:27:16 -0500

    porter commi

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    interface ICharacter
+    {
+        string CharacterClass { get; }
+        string Name { get; }
+        int Health { get; }
+        void PerformAttack { ICharater target; }
+
+        void RecieveAttack { int damage; }
+
+      
+    }
+}
